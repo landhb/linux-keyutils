ad751ee011166ff3796ecbf89d5dbb6d5616b97f
landhb <landhb@users.noreply.github.com>
2022-09-18 18:28:32 -0400

    Move tests

--- src/keyctl.rs
+++ src/keyctl.rs
@@ -1,4 +1,6 @@
-use crate::{KeyError, KeySerialId};
+use crate::ffi::{keyctl_impl, KeyCtlOperation, KeySerialId};
+use crate::keyctl;
+use crate::KeyError;
 
 /// Rust Interface for KeyCtl operations using the kernel
 /// provided keyrings. Each method is implemented to leverage
@@ -12,7 +14,29 @@ impl KeyCtl {
     }
 
     /// Read the payload data of a key.
-    pub fn read() -> Result<(), KeyError> {
+    pub fn read(&self) -> Result<(), KeyError> {
+        let mut buf = [0u8; 2048];
+        let len = keyctl!(
+            KeyCtlOperation::Read,
+            self.0.as_raw_id() as libc::c_ulong,
+            buf.as_mut_ptr() as _,
+            buf.len() as _
+        )? as usize;
+        println!("{:?}", &buf[..len]);
+        Ok(())
+    }
+
+    pub fn set_perm(&self, perm: u32) -> Result<(), KeyError> {
+        let len = keyctl!(
+            KeyCtlOperation::SetPerm,
+            self.0.as_raw_id() as libc::c_ulong,
+            perm as _
+        )? as usize;
+        Ok(())
+    }
+
+    pub fn clear(&self) -> Result<(), KeyError> {
+        keyctl!(KeyCtlOperation::Clear, self.0.as_raw_id() as libc::c_ulong)?;
         Ok(())
     }
 
@@ -25,5 +49,27 @@ impl KeyCtl {
     /// can be positively instantiated with this operation.
     pub fn update(&self) -> Result<(), KeyError> {
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ffi::{self, KeyType, KeyringIdentifier};
+
+    #[test]
+    fn it_works() {
+        let id = ffi::add_key(
+            KeyType::User,
+            KeyringIdentifier::UserSession,
+            "my-super-secret-test-key",
+            "Test Data".as_bytes(),
+        )
+        .unwrap();
+
+        let keyctl = KeyCtl::from_id(id);
+        //keyctl.set_perm(0x3f3f0000).unwrap();
+        //keyctl.read().unwrap();
+        keyctl.clear().unwrap()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 // no_std CStr/CString support stabilized in Rust 1.64.0
+// CString requires alloc however
 extern crate alloc;
+
+// Internal FFI for raw syscalls
+mod ffi;
 
 // Expose error types
 mod errors;
@@ -11,30 +15,6 @@ pub use errors::KeyError;
 mod keyctl;
 pub use keyctl::KeyCtl;
 
-// Internal FFI for raw syscalls
-mod ffi;
-
 // Expose KeyPermissions API
 mod permissions;
 pub use permissions::KeyPermissions;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let id = add_key(
-            KeyType::User,
-            KeyringIdentifier::UserSession,
-            "my-super-secret-test-key",
-            "Test Data".as_bytes(),
-        )
-        .unwrap();
-
-        let keyctl = KeyCtl::from_id(id);
-        //keyctl.set_perm(0x3f3f0000).unwrap();
-        //keyctl.read().unwrap();
-        keyctl.clear().unwrap()
-    }
-}
